c6c7be42df49c6228482bdf6cd2675663c132fa8
Pea M. Tyczynska <pea@Peas-MacBook-Pro.local>
2016-11-09 13:33:49 +0000

    Test for release bike repsond passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,7 @@
 require 'docking_station_class'
 
-describe 'DockingStation' do
-#it 'DockingStation instances to respond to method release_bike' do
-it {expect(DockingStation.new.release_bike).to eq true}
-#end
+describe DockingStation do
+it { is_expected.to respond_to :release_bike }
 
 
 end
