66b2a7ae8b8f7792d6e3c886086b9800f2afc394
sara <sara@huhbub.com>
2016-11-09 16:52:01 +0000

    step 10

--- lib/docking_station_class.rb
+++ lib/docking_station_class.rb
@@ -1,5 +1,7 @@
+require_relative "bike_class"
+
 class DockingStation
   def release_bike
-
+    Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,14 @@ describe DockingStation do
 it { is_expected.to respond_to :release_bike }
 
 
+it "releases a bike" do
+  expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
+end
+
+it "releases a bike that works" do
+  expect(DockingStation.new.release_bike.working?).to eq true
+end
+
+
+
 end
