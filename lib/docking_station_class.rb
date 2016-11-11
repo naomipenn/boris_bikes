06f67870f90495d2ab4dac16b60cc32d13dd2022
sara <sara@huhbub.com>
2016-11-11 13:13:39 +0000

    step 14

--- lib/docking_station_class.rb
+++ lib/docking_station_class.rb
@@ -2,17 +2,25 @@ require_relative "bike_class"
 
 class DockingStation
 
+  attr_accessor :bikes
+
+  def initialize
+  @bikes = Array.new
+  end
+
+  def dock(bikes)
+    fail 'Docking station is full' if @bikes.length >= capacity
+    @bikes << bikes
+  end
+
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes
   end
 
-
-  def dock(bike)
-    fail 'Docking station is full' if @bike
-    @bike = bike
+  def capacity
+    20
   end
 
-  attr_reader :bike
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,7 @@ require 'docking_station_class'
 
 describe DockingStation do
 station = DockingStation.new
+attr_accessor :station
 
 it { is_expected.to respond_to :release_bike }
 
@@ -9,7 +10,7 @@ it { is_expected.to respond_to :release_bike }
   it "releases a bike" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.release_bike).to eq bike
+    expect(subject.release_bike).to eq [bike]
     end
   end
 
@@ -19,7 +20,7 @@ it { is_expected.to respond_to :release_bike }
 
 it 'docks something' do
   bike = Bike.new
-  expect(subject.dock(bike)).to eq bike
+  expect(subject.dock(bike)).to eq [bike]
 end
 
 
@@ -27,11 +28,15 @@ it 'raises an error when no bikes are available' do
   expect { subject.release_bike }.to raise_error "No bikes available"
   end
 
-it 'raises an error when docking station capacity is full' do
-  bike = Bike.new
-  subject.dock(bike)
-  expect { subject.dock(bike) }.to raise_error "Docking station is full"
-end
+# it 'raises an error when docking station capacity is full' do
+#   bike = Bike.new
+#   subject.dock(bike)
+#   expect { subject.dock(bike) }.to raise_error "Docking station is full"
+# end
 
+it 'has a default capacity of 20 bikes' do
+  20.times {subject.dock Bike.new}
+  expect { subject.dock Bike.new }.to raise_error "Docking station is full"
+end
 
 end
